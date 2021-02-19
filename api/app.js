c1b3257280d8435053af091c5d7999348d2c86de
Bob McHenry <bobmchenryvideo@gmail.com>
2021-02-18 21:03:15 -0800

    Add cors and test data route to express app.js

--- api/app.js
+++ api/app.js
@@ -2,19 +2,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var testdataRouter = require('./routes/testdata');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/build')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/testdata', testdataRouter);
 
 module.exports = app;
